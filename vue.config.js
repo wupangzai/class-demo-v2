f599f319dc48a2c00891a5b6e67f05faa834af99
dingzhen <dingzhen@qq.com>
2025-01-18 12:55:46 +0800

    core: solve the warning of control __VUE_PROD_HYDRATION_MISMATCH_DETAILS__

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
-const { defineConfig } = require('@vue/cli-service')
+const DefinePlugin = require("webpack").DefinePlugin;
+const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      new DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: "false", // 注释掉开发环境的警告
+      }),
+    ],
+  },
+});
