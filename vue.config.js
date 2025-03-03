ea62227eaa9a7d883d4dec709e363619a10b1e56
dingzhen <dingzhen@qq.com>
2025-03-03 17:35:51 +0800

    fix: 修复打包错误

--- vue.config.js
+++ vue.config.js
@@ -7,15 +7,14 @@ module.exports = defineConfig({
     devServer: {
       proxy: {
         "/myclass": {
-          target:
-            "https://bi.innoclass.cn/api/public/card/422e5936-e3f1-4080-a226-e2311ab69019/query", // 目标服务器地址
+          target: "https://bi.innoclass.cn", // 目标服务器地址
           changeOrigin: true, // 是否改变源地址
           pathRewrite: {
             "^/myclass": "", // 重写路径
           },
         },
         "/crm": {
-          target: "https://crm-api.landwave.cn/api/v1", // 目标服务器地址
+          target: "https://crm-api.landwave.cn", // 目标服务器地址
           changeOrigin: true, // 是否改变源地址
           pathRewrite: {
             "^/crm": "", // 重写路径
