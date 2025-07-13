ee45358be4970ec47c51d37df44a4d4589ccc47a
an_an <1530765818@qq.com>
2025-07-13 16:26:43 +0800

    refactor: 重构types-helper.d.ts 文件

--- src/store/index.ts
+++ src/store/index.ts
@@ -11,7 +11,19 @@ import { RootState, EnhancedStore } from "./types/root";
 
 const key: InjectionKey<VuexStore<RootState>> = Symbol(); // 创建一个具备类型安全的唯一注入 Key，用于在 Vue 组件中注入 Store 实例。
 
+const rootState = {
+  version: "v2.0.0",
+} as RootState;
+
 const store = createStore({
+  state: () => rootState,
+
+  mutations: {
+    fn(state, payload: number) {
+      return payload;
+    },
+  },
+
   modules: {
     commonModule,
   },
